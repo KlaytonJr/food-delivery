73d4597648ce32e82230d91473cc3fae2d2406ef
KlaytonJr <klaytonjunior@protonmail.com>
2024-01-12 19:59:45 -0300

    feat: Organize the pages folder

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,15 +1,19 @@
 import { Routes, Route } from 'react-router-dom';
-import Navbar from '../components/Navbar';
-import Main from '../pages/Main';
+import Header from '../components/Header';
+import Home from '../pages/Home';
+import Menu from '../pages/Menu';
 import Checkout from '../pages/Checkout';
+import Orders from '../pages/Orders';
 
 function Router() {
     return (
         <>
-            <Navbar />
+            <Header />
             <Routes>
-                <Route exact path="/" element={<Main />} />
+                <Route exact path="/" element={<Home />} />
+                <Route exact path="/menu" element={<Menu />} />
                 <Route path="/checkout" element={<Checkout />} />
+                <Route path="/orders" element={<Orders />} />
             </Routes>
         </>
     );
