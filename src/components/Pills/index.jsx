e220f9930d6c1d864613524c5e41885a07a46e77
KlaytonJr <klaytonjunior@protonmail.com>
2024-01-12 19:40:41 -0300

    feat: create the base components

---
+++ src/components/Pills/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Pills() {
+  return (
+    <div>Pills</div>
+  )
+}
+
+export default Pills
---
+++ src/components/RestaurantCard/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function RestaurantCard() {
+  return (
+    <div>RestaurantCard</div>
+  )
+}
+
+export default RestaurantCard
